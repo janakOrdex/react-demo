8d15401c6cbeb9ebdad3e6342e38799953581b10
janakOrdex <janak.koradiya@ordextechnology.com>
2023-05-23 13:07:51 +0530

    Adding GitHub Pages

--- src/App.js
+++ src/App.js
@@ -4,20 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      {/* <header className="App-header"> */}
+        <img src={logo} className="App-logo" alt="logo" style={{marginTop:"100px"}}/>
+        <img src={logo} className="App-logo" alt="logo" style={{marginTop:"100px"}}/>
+        <img src={logo} className="App-logo" alt="logo" style={{marginTop:"100px"}}/>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        GitHub Pages with ReactJS
+        </p>       
+      {/* </header> */}
     </div>
   );
 }
